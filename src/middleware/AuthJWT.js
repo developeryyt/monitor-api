2570d4aeaf36b89051a1a766ed4d1f5c49b15605
yeontae <yeontae.y@ingstory.kr>
2024-08-02 17:16:54 +0900

    token

--- src/middleware/AuthJWT.js
+++ src/middleware/AuthJWT.js
@@ -6,15 +6,16 @@ const authJwt = (req, res, next) => {
     const token = req.header('boa-authorization')?.split('Bearer ')[1];
 
     if(!token) {
-        return res.status(401).json({ message: '토큰이 제공되지 않았습니다. '})
+        return res.status(401).json({ message: '토큰이 제공되지 않았습니다.' })
     }
 
     try {
         req.user = verify(token)
         if(!req.user?.['ok']) return res.status(401).json({ message: '유효하지 않은 토큰입니다.' })
+
         next()
     }catch (err) {
-        return res.status(401).json({ message: '유효하지 않은 토큰입니다. '})
+        return res.status(401).json({ message: '유효하지 않은 토큰입니다.' })
     }
 
 }
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const authJWT = require('../../middleware/AuthJWT')
 
 
+// router.post("/shop", async (req, res, next) => {
+//     const mb_id = req.body?.mb_id;
+//     const mb_pw = req.body?.mb_pw;
+//     // if (!mb_id || !mb_pw) return next(createError(400, "mb_id or mb_pw is not exist"));
+//     let response = {};
+//     // try {
+//     //     const _jwtToken = await Shop.findOne({ where: {mb_id, mb_pw} });
+//     //     response["token"] = sign(_jwtToken);
+//     // } catch (e) {
+//     //     return next(createError(500, e.message));
+//     // }
+//     res.json(response);
+// });
 
-router.post("/shop", async (req, res, next) => {
-    const mb_id = req.body?.mb_id;
-    const mb_pw = req.body?.mb_pw;
-    // if (!mb_id || !mb_pw) return next(createError(400, "mb_id or mb_pw is not exist"));
-    let response = {};
-    // try {
-    //     const _jwtToken = await Shop.findOne({ where: {mb_id, mb_pw} });
-    //     response["token"] = sign(_jwtToken);
-    // } catch (e) {
-    //     return next(createError(500, e.message));
-    // }
-    res.json(response);
-});
+router.get('/login',(req, res) => {
+    const params = req.body;
+
+    console.log(params)
+
+})
 
 // router.get("/token", authJWT, async (req, res, next) => {
 //     const params = await req.body;
